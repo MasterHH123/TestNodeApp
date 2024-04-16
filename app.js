a5d4bd69173e7ad12aaf61aceb994dbdf70a4c69
Horacio H <hernandezh4@hotmail.com>
2024-04-16 12:35:07 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const moment = require("moment");
+let i = 5;
+let j = 10;
+
+console.log('Starting...');
+console.log("The sum of i and j is:  " + (i+j));
+
+let date = moment().format("LL");
+console.log("Today's date is: ", date);
